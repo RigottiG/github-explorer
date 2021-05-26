17378f8c6c83fe78a6c00243b89f7001aecfffb0
Guilherme Rigotti <ggrigotti@gmail.com>
2021-05-26 12:35:14 -0300

    Configure css loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,11 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
             }
         ]
     }
