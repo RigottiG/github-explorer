43b202372aaafe32941303a0ffc18c60b51f5d1e
Guilherme Rigotti <ggrigotti@gmail.com>
2021-05-26 12:50:42 -0300

    create RepositoryItem

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,27 +1,13 @@
+import { RepositoryItem } from './RepositoryItem';
+
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">Acessar repositório</a>
-                </li>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">Acessar repositório</a>
-                </li>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">Acessar repositório</a>
-                </li>
+                <RepositoryItem />
+                <RepositoryItem />
             </ul>
         </section>
     )
